4c928bccf3eda0d9f458d1da21dd877b5b66f7ed
t21661 <fbarriola@suppliers.tenaris.com>
2024-07-21 21:46:12 -0300

    Modificacion: Se agrego componente de carta para pokemon y se realizo conexion con PokeAPI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import PokemonCard from "@/components/pokemon-card";
+
 export default function Home() {
+  const pokemon = {
+    id: 1,
+    name: 'Bulbasaur',
+    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+    type: 'grass'
+  };
   return (
+    <div className="container">
     <section className="py-24">
       <div className="container">
         <h1 className="text-3x1 font-bold">
@@ -7,5 +16,7 @@ export default function Home() {
         </h1>
       </div>
     </section>
+    <PokemonCard pokemon={pokemon}/>
+    </div>
   );
 }
---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,55 @@
+ import Image from "next/image";
+
+ interface PokemonCardProps {
+    pokemon:
+    {
+        id: number,
+        name: string,
+        sprite: string,
+        type: string
+    }
+}
+const typeColor = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+  };
+
+export default function PokemonCard({pokemon} : PokemonCardProps)
+{
+    console.log(pokemon);
+    return(
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex justify-center items-center h-64">
+                <Image 
+                className="rounded-t-lg content-center"
+                src={pokemon.sprite} 
+                alt={pokemon.name}
+                width={150}
+                height={150} />
+            </div>
+            <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">
+                    {pokemon.name}
+                </h5>
+                
+                <p className="mb-3 font-normal text-center text-gray-700 dark:text-gray-400">
+                    {pokemon.type}
+                </p>
+            </div>
+        </div>
+    );
+}
